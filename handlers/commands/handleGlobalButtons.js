a79cd13ec1ecb2197786f0e9c48eab88641c2d74
Waxer59 <hnboyano@gmail.com>
2023-02-17 00:22:35 +0100

    feat confirm delete ticket button

--- handlers/commands/handleGlobalButtons.js
+++ handlers/commands/handleGlobalButtons.js
@@ -1,9 +1,28 @@
+const { ActionRowBuilder, ButtonBuilder } = require('@discordjs/builders')
+const { ButtonStyle } = require('discord.js')
+
+const DELETE_TICKET_CONFIRMATION_COMPONENTS =
+  new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+      .setCustomId('close-ticket-confirm')
+      .setLabel('✅ Confirm')
+      .setDisabled(false)
+      .setStyle(ButtonStyle.Secondary)
+  )
+
 const handleGlobalButtons = (client, interaction) => {
   const buttonId = interaction.customId
 
   switch (buttonId) {
-    case 'close-ticket':
+    case 'close-ticket-confirm':
       interaction.channel.delete()
+      break
+    case 'close-ticket':
+      interaction.reply({
+        content: '**You are about to delete the ticket, are you sure?**',
+        components: [DELETE_TICKET_CONFIRMATION_COMPONENTS],
+        ephemeral: true
+      })
       break
   }
 }
